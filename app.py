6f7744dfe5da454b39bb24e3df1c2d2836f4fe7e
shinnosuke1111 <2023jfr-ic-student001@la-study.com>
2023-05-24 16:51:31 +0900

    {first_cord}

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask
-
+from flask import Flask, request, render_template
+from datetime import datetime
+from flask_sqalchemy import SQQLAlchemy
 app = Flask(__name__)
 
+class Article(db.Model):
+  id = db.Column(db.Integer, primary_key=True, autincrement=True)
+  pub_date = db.Column(db.DateTime, nullable=False, default=datetime.utnow)
+  name = db.Column(db.Text())
+  article = db.Column(db.Text())
+
+
 @app.route('/')
-def index(word=None):
-    if word:
-      return word
-    else:
-      return 'Hello World'
+def bbs():
+  message = 'Hello'
+  return render_template('index.html', message = message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+  app.run(debug=True)
 
